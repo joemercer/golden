b3389f6a38ce00034238ccd97aefedee4831d5a2
Joe Mercer <mynameisjoemercer@gmail.com>
2014-01-01 14:13:55 -0800

    added links and more consistency

--- tests/tests.js
+++ tests/tests.js
@@ -1,38 +1,38 @@
-$(function(){
+// $(function(){
 
-	var $header = $('.main-header-container');
-	var $nav = $('.nav-container');
+// 	var $header = $('.main-header-container');
+// 	var $nav = $('.nav-container');
 
-	var start = $header.scrollTop();
-	var end = 70;
+// 	var start = $header.scrollTop();
+// 	var end = 70;
 
-	var scrolled;
-	var before = true;
-	var after = false;
-	$(document).scroll(function(e){
-		scrolled = $(document).scrollTop();
+// 	var scrolled;
+// 	var before = true;
+// 	var after = false;
+// 	$(document).scroll(function(e){
+// 		scrolled = $(document).scrollTop();
 
-		// change to header
-		if (after && scrolled < end) {
-			$nav.fadeOut();
-			$header.fadeIn();
+// 		// change to header
+// 		if (after && scrolled < end) {
+// 			$nav.fadeOut();
+// 			$header.fadeIn();
 
-			// $header.toggleClass('hide');
-			// $nav.toggleClass('hide');
-			before = true;
-			after = false;
-		}
+// 			// $header.toggleClass('hide');
+// 			// $nav.toggleClass('hide');
+// 			before = true;
+// 			after = false;
+// 		}
 
-		if (before && scrolled > end) {
-			$header.fadeOut();
-			$nav.fadeIn();
+// 		if (before && scrolled > end) {
+// 			$header.fadeOut();
+// 			$nav.fadeIn();
 
-			// $header.toggleClass('hide');
-			// $nav.toggleClass('hide');
-			before = false;
-			after = true;
-		}
+// 			// $header.toggleClass('hide');
+// 			// $nav.toggleClass('hide');
+// 			before = false;
+// 			after = true;
+// 		}
 
-	});
+// 	});
 
-});
+// });
